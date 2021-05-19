c2ce2055872e431b36c0df349e9d6398fc4f058e
keicam123 <80455702+keicam123@users.noreply.github.com>
2021-05-19 20:40:01 +0200

    update1
    platforma, gamecontroller

---
+++ Assets/platform.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platform : MonoBehaviour
+{
+    public float speed = 3f;
+    public bool mRight = true;
+
+    void Update()
+    {
+        if (transform.position.x > 33)
+        {
+            mRight = false;
+        }
+        if (transform.position.x < 25)
+        {
+            mRight = true;
+        }
+        if (mRight)
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+    }
+    
+}
