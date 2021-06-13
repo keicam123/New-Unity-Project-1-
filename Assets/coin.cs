ebabc7718daa825cb8b3f1fcf4326d20eaffa040
keicam123 <80455702+keicam123@users.noreply.github.com>
2021-06-13 12:50:58 +0200

    update

--- Assets/coin.cs
+++ Assets/coin.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class coin : PlayerPickable
 {
-
+    public int coinValue = 1;
     protected override void pickUpItem(GameObject player)
     {
-        player.GetComponent<CharacterStats>().Score += 100;
+        player.GetComponent<CharacterStats>().Score += coinValue;
         GameObject.Destroy(this.gameObject);
     }
 }
--- Assets/spiker.cs
+++ Assets/spiker.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class spiker : MonoBehaviour
 {
-    public float speed;
+    public float speed = 0.5f;
     public bool mRight;
-
+    private float boostTime;
+    private bool boosting = false;
     void Update()
     {
       if(mRight)
@@ -16,6 +17,18 @@ public class spiker : MonoBehaviour
         else
         {
             transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+        }
+
+        if (boosting)
+        {
+            boostTime += Time.deltaTime;
+            if(boostTime > 1)
+            {
+                speed = 0.5f;
+                boostTime = 0;
+                boosting = false;
+            }
+
         }
     }
     void OnTriggerEnter2D(Collider2D spike)
@@ -31,6 +44,18 @@ public class spiker : MonoBehaviour
                 mRight = true;
             }
         }
-
+        if (spike.gameObject.CompareTag("Player"))
+        {
+            boosting = true;
+            speed += 0.25f;
+            if (mRight == true)
+            {
+                mRight = false;
+            }
+            else
+            {
+                mRight = true;
+            }
+        }
     }
 }
